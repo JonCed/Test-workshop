74d7cdf8dd77e6a6a2fd5c6d9ac8decd3d7133ee
Jonatan Cederberg <jonceder@student.chalmers.se>
2023-11-01 15:40:06 +0100

    Test - NewBracnh - toGit

--- src/main/java/group/ex/Main.java
+++ src/main/java/group/ex/Main.java
@@ -2,7 +2,7 @@ package group.ex;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hld!");
     }
 
     int x = 1;
